8f15975df0ce9a8cb2e2846a7ed6fde931c0ab56
c14006078 <c14006078@mail.ncku.edu.tw>
2016-09-24 03:49:19 +0800

    Decrease the phonebook struct size to enhance the cache hit.

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "phonebook_opt.h"
 
@@ -6,10 +7,23 @@
 entry *findName(char lastname[], entry *pHead)
 {
     /* TODO: implement */
+		while (pHead != NULL) {
+    	if (strcasecmp(lastname, pHead->lastName) == 0)
+        return pHead;
+        pHead = pHead->pNext;
+    }
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
+
+		e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
+
+    return e;
+
     return NULL;
 }
--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,9 +5,9 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+#define OPT 1
+
+typedef struct _detail{
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,7 +17,14 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+}detail;
+
+typedef detail *pdetail;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
     struct __PHONE_BOOK_ENTRY *pNext;
+		pdetail dtl;
 } entry;
 
 entry *findName(char lastname[], entry *pHead);
