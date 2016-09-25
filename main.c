0b7dfd5963d99f24269e46fdd05dfb6dcdf200f4
c14006078 <c14006078@mail.ncku.edu.tw>
2016-09-26 04:49:28 +0800

    Keep the same coding style by "astyle --style=kr --indent=tab --indent-switches --suffix=none"

--- main.c
+++ main.c
@@ -10,103 +10,103 @@
 
 static double diff_in_second(struct timespec t1, struct timespec t2)
 {
-    struct timespec diff;
-    if (t2.tv_nsec-t1.tv_nsec < 0) {
-        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
-        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
-    } else {
-        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
-        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
-    }
-    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+	struct timespec diff;
+	if (t2.tv_nsec-t1.tv_nsec < 0) {
+		diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+		diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+	} else {
+		diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+		diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+	}
+	return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
 }
 
 int main(int argc, char *argv[])
 {
-    FILE *fp;
+	FILE *fp;
 #ifndef OPT
-    int i = 0;
+	int i = 0;
 #endif
-    char line[MAX_LAST_NAME_SIZE];
-    struct timespec start, end;
-    double cpu_time1, cpu_time2;
+	char line[MAX_LAST_NAME_SIZE];
+	struct timespec start, end;
+	double cpu_time1, cpu_time2;
 
-    /* check file opening */
-    fp = fopen(DICT_FILE, "r");
-    if (fp == NULL) {
-        printf("cannot open the file\n");
-        return -1;
-    }
+	/* check file opening */
+	fp = fopen(DICT_FILE, "r");
+	if (fp == NULL) {
+		printf("cannot open the file\n");
+		return -1;
+	}
 
-    /* build the entry */
-    entry *pHead, *e;
-    pHead = (entry *) malloc(sizeof(entry));
-    printf("size of entry : %lu bytes\n", sizeof(entry));
-    e = pHead;
-    e->pNext = NULL;
+	/* build the entry */
+	entry *pHead, *e;
+	pHead = (entry *) malloc(sizeof(entry));
+	printf("size of entry : %lu bytes\n", sizeof(entry));
+	e = pHead;
+	e->pNext = NULL;
 
 #if defined(__GNUC__)
-    __builtin___clear_cache((char *) pHead, (char *) pHead + sizeof(entry));
+	__builtin___clear_cache((char *) pHead, (char *) pHead + sizeof(entry));
 #endif
 
 #if defined(OPT)
-    clock_gettime(CLOCK_REALTIME, &start);
-		while (fgets(line, sizeof(line), fp))
-      e = append(line, e);
-    clock_gettime(CLOCK_REALTIME, &end);
-    cpu_time1 = diff_in_second(start, end);
+	clock_gettime(CLOCK_REALTIME, &start);
+	while (fgets(line, sizeof(line), fp))
+		e = append(line, e);
+	clock_gettime(CLOCK_REALTIME, &end);
+	cpu_time1 = diff_in_second(start, end);
 
 #else
-   clock_gettime(CLOCK_REALTIME, &start);
-   while (fgets(line, sizeof(line), fp)) {
-       while (line[i] != '\0')
-            i++;
-        line[i - 1] = '\0';
-        i = 0;
-        e = append(line, e);
-    }
+	clock_gettime(CLOCK_REALTIME, &start);
+	while (fgets(line, sizeof(line), fp)) {
+		while (line[i] != '\0')
+			i++;
+		line[i - 1] = '\0';
+		i = 0;
+		e = append(line, e);
+	}
 
-    clock_gettime(CLOCK_REALTIME, &end);
-    cpu_time1 = diff_in_second(start, end);
+	clock_gettime(CLOCK_REALTIME, &end);
+	cpu_time1 = diff_in_second(start, end);
 
 #endif
 
-    /* close file as soon as possible */
-    fclose(fp);
+	/* close file as soon as possible */
+	fclose(fp);
 
-    e = pHead;
+	e = pHead;
 
-    /* the givn last name to find */
-    char input[MAX_LAST_NAME_SIZE] = "zyxel";
-    e = pHead;
+	/* the givn last name to find */
+	char input[MAX_LAST_NAME_SIZE] = "zyxel";
+	e = pHead;
 
-    assert(findName(input, e) &&
-           "Did you implement findName() in " IMPL "?");
-    assert(0 == strcmp(findName(input, e)->lastName, "zyxel"));
+	assert(findName(input, e) &&
+	       "Did you implement findName() in " IMPL "?");
+	assert(0 == strcmp(findName(input, e)->lastName, "zyxel"));
 
 #if defined(__GNUC__)
-    __builtin___clear_cache((char *) pHead, (char *) pHead + sizeof(entry));
+	__builtin___clear_cache((char *) pHead, (char *) pHead + sizeof(entry));
 #endif
-    /* compute the execution time */
-    clock_gettime(CLOCK_REALTIME, &start);
-    findName(input, e);
-    clock_gettime(CLOCK_REALTIME, &end);
-    cpu_time2 = diff_in_second(start, end);
+	/* compute the execution time */
+	clock_gettime(CLOCK_REALTIME, &start);
+	findName(input, e);
+	clock_gettime(CLOCK_REALTIME, &end);
+	cpu_time2 = diff_in_second(start, end);
 
-    FILE *output;
+	FILE *output;
 #if defined(OPT)
-    output = fopen("opt.txt", "a");
+	output = fopen("opt.txt", "a");
 #else
-    output = fopen("orig.txt", "a");
+	output = fopen("orig.txt", "a");
 #endif
-    fprintf(output, "append() findName() %lf %lf\n", cpu_time1, cpu_time2);
-    fclose(output);
+	fprintf(output, "append() findName() %lf %lf\n", cpu_time1, cpu_time2);
+	fclose(output);
 
-    printf("execution time of append() : %lf sec\n", cpu_time1);
-    printf("execution time of findName() : %lf sec\n", cpu_time2);
+	printf("execution time of append() : %lf sec\n", cpu_time1);
+	printf("execution time of findName() : %lf sec\n", cpu_time2);
 
-    if (pHead->pNext) free(pHead->pNext);
-    free(pHead);
+	if (pHead->pNext) free(pHead->pNext);
+	free(pHead);
 
-    return 0;
+	return 0;
 }
--- phonebook_orig.c
+++ phonebook_orig.c
@@ -8,21 +8,21 @@
 /* original version */
 entry *findName(char lastname[], entry *pHead)
 {
-    while (pHead != NULL) {
-        if (strcasecmp(lastname, pHead->lastName) == 0)
-            return pHead;
-        pHead = pHead->pNext;
-    }
-    return NULL;
+	while (pHead != NULL) {
+		if (strcasecmp(lastname, pHead->lastName) == 0)
+			return pHead;
+		pHead = pHead->pNext;
+	}
+	return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
-    /* allocate memory for the new entry and put lastName */
-    e->pNext = (entry *) malloc(sizeof(entry));
-    e = e->pNext;
-    strcpy(e->lastName, lastName);
-    e->pNext = NULL;
+	/* allocate memory for the new entry and put lastName */
+	e->pNext = (entry *) malloc(sizeof(entry));
+	e = e->pNext;
+	strcpy(e->lastName, lastName);
+	e->pNext = NULL;
 
-    return e;
+	return e;
 }
