e752c8f1cc560fda08626bb88845b8045a86d59f
lankudot <airlanser@gmail.com>
2016-10-05 03:17:06 +0800

    Modulize the text_align

--- text_align.c
+++ text_align.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
+#include "text_align.h"
 
 #define MAX_BUFF_SIZE 100
 
@@ -15,43 +16,42 @@ static off_t fsize(char *path);
  * @brief Pad each line of the text file _origiFileName_
  *  to _PadToLen_ characters and write to the file _alignedFileName_.
  */
-int main(int argc, char *argv[])
+int text_align(const char *originFileName,
+               const char *alignedFileName,
+               const int padToLen)
 {
-    assert(argc == 4 &&
-           "./main <originFileName> <alignedFileName> <PadToLen>");
+    printf("orginal file size = %ld\n", fsize(originFileName));
 
-    char *org = argv[1];
-    char *mod = argv[2];
-    int pad = atoi(argv[3]);
+    FILE *fd_origin = fopen(originFileName, "r");
+    FILE *fd_align = fopen(alignedFileName, "w+");
 
-    printf("orginal file size = %ld\n", fsize(org));
-
-    FILE *fd0 = fopen(org, "r");
-    FILE *fd1 = fopen(mod, "w+");
+    // Fail to open the file
+    if (!fd_origin || !fd_align)
+        return -1;
 
     char rbuf[MAX_BUFF_SIZE];
     int suffix;
 
-    char *wbuf = (char *) malloc(sizeof(char) * pad);
+    char *wbuf = (char *) malloc(sizeof(char) * padToLen);
 
-    // Read each line from input file and pad to "pad" characters.
-    while (fgets(rbuf, sizeof(rbuf), fd0)) {
-        memset(wbuf, '\0', pad);
+    // Read each line from input file and pad to "padToLen" characters.
+    while (fgets(rbuf, sizeof(rbuf), fd_origin)) {
+        memset(wbuf, '\0', padToLen);
 
-        suffix = pad - strlen(rbuf);
+        suffix = padToLen - strlen(rbuf);
         // The length of input text is longer than the length to padding to.
         // Warn the user, and still write to the output file but with only
         // first "PadToLen" bytes.
         if (suffix < 0)
             printf("Warning:"
-                   " The length of %s is longer than %d.\n", rbuf, pad);
+                   " The length of %s is longer than %d.\n", rbuf, padToLen);
         strncpy(wbuf, rbuf, strlen(rbuf));
 
-        fwrite(wbuf, pad, 1, fd1);
+        fwrite(wbuf, padToLen, 1, fd_align);
     }
 
-    fclose(fd0);
-    fclose(fd1);
+    fclose(fd_align);
+    fclose(fd_origin);
     free(wbuf);
 
     return 0;
---
+++ text_align.h
@@ -0,0 +1,8 @@
+#ifndef __TEXT_ALIGN_
+#define __TEXT_ALIGN_
+
+int text_align(const char *originFileName,
+               const char *alignedFileName,
+               const int padToLen);
+
+#endif // __TEXT_ALIGN_
